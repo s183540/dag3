baff5ae340b14cb9390909cc63e28f858eddeb66
Eline Siegumfeldt <s183540@dtu.dk>
2025-01-14 11:29:13 +0100

    added pytest skip if data dir not there

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,10 +1,12 @@
 from torch.utils.data import Dataset
 from elines_pakke.data import corrupt_mnist
 import pdb
+import os.path
+import pytest
 import torch
 
 
-
+@pytest.mark.skipif(not os.path.exists("./data"), reason="Data files not found")
 def test_my_dataset():
     # """Test the MyDataset class."""
     # dataset = MyDataset("data/raw")
